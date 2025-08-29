bdf6e69aad148abf0e75e4095f09807d3b7bb48f
Chandan Agrawal <cagrawal0012@gmail.com>
2025-08-29 22:29:05 +0530

    chapter-2 notes added

---
+++ Chapter 2 - Comments and Java tokens/chapter2--comments and java tokens/1.comments/Addition.java
@@ -0,0 +1,58 @@
+/**
+ * This class is used for adding two numbers
+ * @author : HK
+ * @since: 1.0
+ */
+public class Addition {
+   /*
+    public static void add() {
+        int a = 10;
+	int b = 20;
+	int c = a + b;
+ 	System.out.println(c);
+    }
+   */
+
+    public static void add(int a, int b) {
+	int c = a + b;
+
+ 	//System.out.println(c);
+	System.out.println("The addition result is: " + c);
+
+    } //add close
+
+    public static void main(String[] args) {
+        System.out.println("main start");
+		//add();
+
+		add(10, 20);
+		add(30, 40);
+		add(12330, 5465640);
+       
+	System.out.println("main end");
+
+    } //main close
+
+} //class close 
+
+/*
+Save above program with the name "Addition.java" 
+in a new folder "02Comments and Java Tokens"
+D:\FSJD\01CJ\
+   01JB
+    Example.java
+    NamePrinter.java
+    NamePrinterWithStars.java
+
+   02Comments and Java Tokens
+    Addition.java
+*/
+
+
+
+
+
+
+
+
+
